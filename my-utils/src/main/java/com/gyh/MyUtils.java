18ad3f8f9e12bc5bedcea41d9f1ee6adbedbbb98
螺丝钉 <32859363+gyh03@users.noreply.github.com>
2018-10-17 20:42:02 +0800

    utils

---
+++ my-utils/src/main/java/com/gyh/MyUtils.java
@@ -0,0 +1,11 @@
+package com.gyh;
+
+/**
+ * @author guoyanhong
+ * @date 2018/9/14 17:31
+ */
+public class MyUtils {
+    public static void main(String[] args) {
+        System.out.println(" main ...");
+    }
+}
